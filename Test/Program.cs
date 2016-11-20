8dea5d6ca5ab0d100210d4ae083f654ab055deac
naresh.panta@gmail.com <naresh.panta@gmail.com>
2016-11-20 18:45:23 +0000

    added extra lines in the program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -9,9 +9,12 @@ namespace Test
 			Animal princess = new Dog();
 			Animal fluffy = new Cat();
 			Animal sparrow = new Sparrow();
+			Animal sparrow2 = new Sparrow();
+
 			princess.talk();
 			fluffy.talk();
 			sparrow.talk();
+			sparrow2.talk();
 		}
 	}
 }
